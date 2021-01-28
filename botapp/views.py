41f25efb83278c2c8c77ddea7a5cc30791eea257
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-01-28 14:17:37 +0800

    MessageEvent

--- botapp/views.py
+++ botapp/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import *
 
 #models.py資料表
 from botapp.models import *
@@ -19,8 +19,13 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
+        #先設定一個要回傳的message空集合
+        message=[]
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
+        
+        #在這裡將body寫入機器人回傳的訊息中，可以更容易看出你收到的webhook長怎樣#
+        message.append(TextSendMessage(text=str(body)))
 
         try:
             events = parser.parse(body, signature)
@@ -30,24 +35,63 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
+            #如果事件為訊息
             if isinstance(event, MessageEvent):
-                mtext=event.message.text
-                uid=event.source.user_id
-                profile=line_bot_api.get_profile(uid)
-                name=profile.display_name
-                pic_url=profile.picture_url
+                print(event.message.type)
+                if event.message.type=='text':
+                    message.append(TextSendMessage(text='文字訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
 
-                message=[]
-                if User_Info.objects.filter(uid=uid).exists()==False:
-                    User_Info.objects.create(uid=uid,name=name,pic_url=pic_url,mtext=mtext)
-                    message.append(TextSendMessage(text='會員資料新增完畢'))
-                elif User_Info.objects.filter(uid=uid).exists()==True:
-                    message.append(TextSendMessage(text='已經有建立會員資料囉'))
-                    user_info = User_Info.objects.filter(uid=uid)
-                    for user in user_info:
-                        info = 'UID=%s\nNAME=%s\n大頭貼=%s'%(user.uid,user.name,user.pic_url)
-                        message.append(TextSendMessage(text=info))
+                elif event.message.type=='image':
+                    message.append(TextSendMessage(text='圖片訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
+
+                elif event.message.type=='location':
+                    message.append(TextSendMessage(text='位置訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
+
+                elif event.message.type=='video':
+                    message.append(TextSendMessage(text='影片訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
+
+
+                elif event.message.type=='sticker':
+                    message.append(TextSendMessage(text='貼圖訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
+
+                elif event.message.type=='audio':
+                    message.append(TextSendMessage(text='聲音訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
+
+                elif event.message.type=='file':
+                    message.append(TextSendMessage(text='檔案訊息'))
+                    line_bot_api.reply_message(event.reply_token,message)
+
+            elif isinstance(event, FollowEvent):
+                print('加入好友')
                 line_bot_api.reply_message(event.reply_token,message)
+
+            elif isinstance(event, UnfollowEvent):
+                print('取消好友')
+
+            elif isinstance(event, JoinEvent):
+                print('進入群組')
+                line_bot_api.reply_message(event.reply_token,message)
+
+            elif isinstance(event, LeaveEvent):
+                print('離開群組')
+                line_bot_api.reply_message(event.reply_token,message)
+
+            elif isinstance(event, MemberJoinedEvent):
+                print('有人入群')
+                line_bot_api.reply_message(event.reply_token,message)
+
+            elif isinstance(event, MemberLeftEvent):
+                print('有人退群')
+                line_bot_api.reply_message(event.reply_token,message)
+
+            elif isinstance(event, PostbackEvent):
+                print('PostbackEvent')
 
         return HttpResponse()
     else:
